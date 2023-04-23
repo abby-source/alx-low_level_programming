e79aab44959a33a11cfa45816842233a0d044efd
abby-source <marangatabitha@gmail.com>
2023-04-23 03:30:09 -0700

    My favourite first line of C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A function that prints my first line of code
- * Return: always 0 (success)
+ * main - Afunction that prints my first line of code
+ * Return: Always 0 (success)
  */
 int main(void)
 {
