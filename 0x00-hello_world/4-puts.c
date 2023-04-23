897fa921c8dd83bb1758c1706e62fce4653be007
abby-source <marangatabitha@gmail.com>
2023-04-23 00:07:43 -0700

    My first line of code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -A program that prints my first line of code
+ * return: 0 (success)
+ * /
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
