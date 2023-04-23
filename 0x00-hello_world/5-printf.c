5f84ca4228cb875d46011ff45abb71c8605318ce
abby-source <marangatabitha@gmail.com>
2023-04-23 11:27:54 -0700

    My first printf line of code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - A program that prints printf function
- * return: Always 0 (success)
+ * main - A function that prits a line of code
+ * Return: Always 0 (success)
  */
 int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+{	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0);
 }
